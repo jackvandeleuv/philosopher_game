16ff2fad9ca13c7d5523b4ff5f5b28d7a5c7841d
Jack Vandeleuv <jvandele@andrew.cmu.edu>
2023-05-20 23:29:35 -0400

    refactor: added methods to get phil positions

--- dist/scenes/DefaultScene.js
+++ dist/scenes/DefaultScene.js
@@ -5,40 +5,44 @@ export class DefaultScene {
         this.phil2 = phil2;
         this.imageRepo = imageRepo;
         this.sceneComplete = true;
+        this.x = this.ctx.canvas.width / 6.4;
+        this.y = this.ctx.canvas.width / 3.6;
+        this.w = this.ctx.canvas.width / 3.6;
+        this.h = this.ctx.canvas.width / 3.6;
+        this.x2 = this.ctx.canvas.width / 1.55;
+        this.y2 = this.ctx.canvas.width / 9;
+        this.w2 = this.ctx.canvas.width / 5.4;
+        this.h2 = this.ctx.canvas.width / 5.4;
     }
     render() {
-        let x = this.ctx.canvas.width / 6.4;
-        let y = this.ctx.canvas.width / 3.6;
-        let w = this.ctx.canvas.width / 3.6;
-        let h = this.ctx.canvas.width / 3.6;
-        this.drawPlatform(x + (w / 2), y + h, w * .8, h * .2);
+        this.drawPlatform(this.x + (this.w / 2), this.y + this.h, this.w * .8, this.h * .2);
         let image1 = this.imageRepo.getImage(this.phil1.getImagePath());
         if (image1 != null && !this.phil1.isRetired()) {
-            this.ctx.clearRect(x, y, w, h);
-            this.ctx.drawImage(image1, x, y, w, h);
+            this.ctx.drawImage(image1, this.x, this.y, this.w, this.h);
         }
-        let x2 = this.ctx.canvas.width / 1.55;
-        let y2 = this.ctx.canvas.width / 9;
-        let w2 = this.ctx.canvas.width / 5.4;
-        let h2 = this.ctx.canvas.width / 5.4;
-        this.drawPlatform(x2 + (w2 / 2), y2 + h2, w * .8, h * .2);
+        this.drawPlatform(this.x2 + (this.w2 / 2), this.y2 + this.h2, this.w * .8, this.h * .2);
         let image2 = this.imageRepo.getImage(this.phil2.getImagePath());
         if (image2 != null && !this.phil2.isRetired()) {
-            // this.ctx.clearRect(x2, y2, w2, h2);
             // Save the context state
             this.ctx.save();
             // Move to the center of the image, scale it negatively on x axis, then move it back
-            this.ctx.translate(x2 + w2 / 2, 0);
+            this.ctx.translate(this.x2 + this.w2 / 2, 0);
             this.ctx.scale(-1, 1);
-            this.ctx.translate(-(x2 + w2 / 2), 0);
+            this.ctx.translate(-(this.x2 + this.w2 / 2), 0);
             // Draw the mirrored image
-            this.ctx.drawImage(image2, x2, y2, w2, h2);
+            this.ctx.drawImage(image2, this.x2, this.y2, this.w2, this.h2);
             // Restore the context to the previous state
             this.ctx.restore();
         }
         if (this.phil1.getImagePath() && this.phil2.getImagePath()) {
             this.sceneComplete = true;
         }
+    }
+    getPhil1Position() {
+        return [this.x, this.y];
+    }
+    getPhil2Position() {
+        return [this.x2, this.y2];
     }
     drawPlatform(x, y, radiusX, radiusY) {
         this.ctx.beginPath();
--- src/scenes/DefaultScene.ts
+++ src/scenes/DefaultScene.ts
@@ -4,6 +4,14 @@ import { ImageRepository } from '../ImageRepository.js';
 
 export class DefaultScene implements GameScene {
     private sceneComplete: boolean = true;
+    private x = this.ctx.canvas.width / 6.4;
+    private y = this.ctx.canvas.width / 3.6;
+    private w = this.ctx.canvas.width / 3.6;
+    private h = this.ctx.canvas.width / 3.6;
+    private x2 = this.ctx.canvas.width / 1.55;
+    private y2 = this.ctx.canvas.width / 9;
+    private w2 = this.ctx.canvas.width / 5.4;
+    private h2 = this.ctx.canvas.width / 5.4;
 
     constructor(private ctx: CanvasRenderingContext2D, 
                 private phil1: Philosopher,
@@ -11,39 +19,27 @@ export class DefaultScene implements GameScene {
                 private imageRepo: ImageRepository) {}
 
     render(): void {
-        let x = this.ctx.canvas.width / 6.4;
-        let y = this.ctx.canvas.width / 3.6;
-        let w = this.ctx.canvas.width / 3.6;
-        let h = this.ctx.canvas.width / 3.6;
-        this.drawPlatform(x + (w / 2), y + h, w * .8, h * .2);
+        this.drawPlatform(this.x + (this.w / 2), this.y + this.h, this.w * .8, this.h * .2);
 
         let image1 = this.imageRepo.getImage(this.phil1.getImagePath());
         if (image1 != null && !this.phil1.isRetired()) {
-            this.ctx.clearRect(x, y, w, h);
-            this.ctx.drawImage(image1, x, y, w, h);
+            this.ctx.drawImage(image1, this.x, this.y, this.w, this.h);
         }
 
-
-        let x2 = this.ctx.canvas.width / 1.55;
-        let y2 = this.ctx.canvas.width / 9;
-        let w2 = this.ctx.canvas.width / 5.4;
-        let h2 = this.ctx.canvas.width / 5.4;
-        this.drawPlatform(x2 + (w2 / 2), y2 + h2, w * .8, h * .2);
+        this.drawPlatform(this.x2 + (this.w2 / 2), this.y2 + this.h2, this.w * .8, this.h * .2);
 
         let image2 = this.imageRepo.getImage(this.phil2.getImagePath());
-        if (image2 != null && !this.phil2.isRetired()) {
-            // this.ctx.clearRect(x2, y2, w2, h2);
-        
+        if (image2 != null && !this.phil2.isRetired()) {      
             // Save the context state
             this.ctx.save(); 
             
             // Move to the center of the image, scale it negatively on x axis, then move it back
-            this.ctx.translate(x2 + w2 / 2, 0); 
+            this.ctx.translate(this.x2 + this.w2 / 2, 0); 
             this.ctx.scale(-1, 1);
-            this.ctx.translate(-(x2 + w2 / 2), 0);
+            this.ctx.translate(-(this.x2 + this.w2 / 2), 0);
             
             // Draw the mirrored image
-            this.ctx.drawImage(image2, x2, y2, w2, h2);
+            this.ctx.drawImage(image2, this.x2, this.y2, this.w2, this.h2);
             
             // Restore the context to the previous state
             this.ctx.restore(); 
@@ -52,6 +48,14 @@ export class DefaultScene implements GameScene {
         if (this.phil1.getImagePath() && this.phil2.getImagePath()) {
             this.sceneComplete = true;
         }
+    }
+
+    getPhil1Position(): number[] {
+        return [this.x, this.y];
+    }
+
+    getPhil2Position(): number[] {
+        return [this.x2, this.y2];
     }
 
     private drawPlatform(x: number, y: number, radiusX: number, radiusY: number): void {
